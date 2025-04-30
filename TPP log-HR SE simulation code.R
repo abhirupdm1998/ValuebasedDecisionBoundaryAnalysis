88494a6feb16eeaf2cc27c6386293e31f2e2781a
Abhirup Dutta Majumdar, MSc <94047210+abhirupdm1998@users.noreply.github.com>
2025-04-30 14:30:00 +0530

    Update TPP log-HR SE simulation code.R

--- TPP log-HR SE simulation code.R
+++ TPP log-HR SE simulation code.R
@@ -1,5 +1,6 @@
-Created by Abhirup Dutta Majumdar
-Quality checked by Sekhar Kumar Dutta 
+# © Copyright reserved by the Authors and PharmaQuant Insights Pvt. Ltd, India @2025
+# Created by Abhirup Dutta Majumdar
+# Quality checked by Sekhar Kumar Dutta 
 # Note: The user should always fully credit the authors of this paper in every private and public work conducted. If ever found in violation, there could be legal and copyright infringement consequences
 ##### Estimation of standard error######
 setwd("") #add your directory
